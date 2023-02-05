faa9190b1e2c887bcd7bc9c1677419135098a56e
Sergey Lukovkin <slukovkin@icloud.com>
2023-02-05 22:54:11 +0200

    add change title for button create task

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import NewTask from "./components/NewTask/NewTask"
 import TodoList from "./components/TodoList/TodoList"
 import "./App.css"
@@ -6,28 +6,25 @@ import "./App.css"
 const App = () => {
   const [visible, setVisible] = useState(false)
   const [todos, setTodos] = useState([])
+  const [titleBtn, setTitleBtn] = useState("Создать новую задачу")
   const message = "Пока нет созданных задач"
-  const content_btn = "Создать новую задачу"
+
+  useEffect(() => {
+    visible ? setTitleBtn("Вернуться к списку задач") : setTitleBtn("Создать новую задачу")
+  }, [visible])
 
   const addTask = (newTask) => {
-    console.log(newTask);
-    if(newTask) {
+    if (newTask) {
       setTodos([...todos, newTask])
     }
     setVisible(!visible)
   }
 
-  const showTask = () => {
-    
-  }
+  const showTask = () => {}
 
-  const removeTask = () => {
+  const removeTask = () => {}
 
-  }
-
-  const updateTask = () => {
-
-  }
+  const updateTask = () => {}
 
   const createNewTask = () => {
     setVisible(!visible)
@@ -36,10 +33,14 @@ const App = () => {
     <>
       <h2>Задач в списке {todos.length}</h2>
       <button className='create_task' onClick={createNewTask}>
-        {content_btn}
+        {titleBtn}
       </button>
-      {visible && <NewTask addTask = {addTask}/>}
-      {todos.length !== 0 && !visible ? <TodoList todos={todos} /> : <h4>{message}</h4>}
+      {visible && <NewTask addTask={addTask} />}
+      {todos.length !== 0 && !visible ? (
+        <TodoList todos={todos} />
+      ) : (
+        <h4>{message}</h4>
+      )}
     </>
   )
 }
